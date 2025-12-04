b83a473b8732b0dbfab22b978de945d8fd97b7f5
ChristineKanga-VF <christinek@victoryfarmskenya.com>
2025-12-04 12:01:12 +0300

    fix: feedback changes

--- victory_group_custom_app/victory_group_erpnext_custom_app/doctype/incident_report/incident_report.py
+++ victory_group_custom_app/victory_group_erpnext_custom_app/doctype/incident_report/incident_report.py
@@ -2,6 +2,7 @@
 # For license information, please see license.txt
 
 import frappe
+from frappe.utils import today
 from frappe.model.document import Document
 from hrms.hr.utils import set_geolocation_from_coordinates
 from frappe import _
@@ -9,6 +10,9 @@ from frappe import _
 class IncidentReport(Document):
 	def validate(self):
 		self.set_geolocation()
+		
+		if self.date_and_time and self.date_and_time > today():
+				frappe.throw("Date cannot be in the future.")
 		
 	@frappe.whitelist()
 	def set_geolocation(self):
@@ -21,3 +25,4 @@ class IncidentReport(Document):
 		
 		if not attachments:
 			frappe.throw(_("Please Attach Reference Document(s)."))
+
