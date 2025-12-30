ea561c203a03561d636cd07d483114092f4bd5f4
ChristineKanga-VF <christinek@victoryfarmskenya.com>
2025-12-30 12:27:19 +0300

    fix: changes

--- victory_group_custom_app/victory_group_erpnext_custom_app/doctype/incident_report/incident_report.js
+++ victory_group_custom_app/victory_group_erpnext_custom_app/doctype/incident_report/incident_report.js
@@ -9,8 +9,8 @@ frappe.ui.form.on("Incident Report", {
 
 function update_severity_options(frm) {
 	const restricted_types = ["Near Miss (NM)", "Others"];
-	const restricted_options = "\nModerate\nHigh\nCatastrophic";
-	const default_options = "\nLow\nModerate\nHigh\nCatastrophic\nLow Catastrophic";
+	const restricted_options = "\nLow\nModerate\nHigh\nCatastrophic";
+	const default_options = "\nLow\nModerate\nHigh\nCatastrophic";
 
 	if (restricted_types.includes(frm.doc.incident_type)) {
 		frm.set_df_property('severity', 'options', restricted_options);
