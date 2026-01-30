e010ebdec0ea8aa9e8030854c4b511c648ffc478
uwizerah <u.kabasinga@gmail.com>
2026-01-30 10:31:53 +0200

    feat: others incident remove victim reqs

--- victory_group_custom_app/victory_group_erpnext_custom_app/doctype/incident_report/incident_report.js
+++ victory_group_custom_app/victory_group_erpnext_custom_app/doctype/incident_report/incident_report.js
@@ -1,53 +1,82 @@
 // Copyright (c) 2025, Christine Kanga and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("Incident Report", {
-	fetch_geolocation: (frm) => {
-		hrms.fetch_geolocation(frm);
-	},
-});
+const VICTIM_FIELDS = ['role_type', 'victim', 'victim_employee', 'victim_employee_department', 
+	'victim_presence_status', 'ppe_status', 'department'];
+const SEVERITY_MAP = { "First Aid Case (FAC)": "Moderate", "Lost Time Injury (LTI)": "High", "Fatality": "Catastrophic" };
 
-function update_severity_options(frm) {
-	const restricted_types = ["Near Miss (NM)", "Others"];
-	const restricted_options = "\nLow\nModerate\nHigh\nCatastrophic";
-	const default_options = "\nLow\nModerate\nHigh\nCatastrophic";
+function set_field_props(frm, fields, hidden, reqd) {
+	fields.forEach(f => {
+		frm.set_df_property(f, 'hidden', hidden);
+		frm.set_df_property(f, 'reqd', reqd);
+	});
+	fields.forEach(f => frm.refresh_field(f));
+}
 
-	if (restricted_types.includes(frm.doc.incident_type)) {
-		frm.set_df_property('severity', 'options', restricted_options);
-		const allowed = ['Low', 'Moderate', 'High', 'Catastrophic'];
-		if (frm.doc.severity && !allowed.includes(frm.doc.severity)) {
-			frm.set_value('severity', null);
-		}
+function toggle_victim_fields(frm) {
+	const is_others = frm.doc.incident_type === 'Others';
+	const subcategory = frm.doc.incident_type_subcategory;
+	
+	if (is_others && (subcategory === 'Asset damage' || subcategory === 'Environmental damage')) {
+		set_field_props(frm, VICTIM_FIELDS, 1, 0);
+	} else if (is_others && subcategory === 'Third-party individual') {
+		set_field_props(frm, ['role_type', 'victim_employee', 'victim_employee_department', 'victim_presence_status', 'ppe_status', 'department'], 1, 0);
+		frm.set_df_property('victim', 'hidden', 0);
+		frm.set_df_property('victim', 'reqd', 1);
+		frm.refresh_field('victim');
+	} else if (is_others) {
+		set_field_props(frm, VICTIM_FIELDS, 1, 0);
 	} else {
-		frm.set_df_property('severity', 'options', default_options);
+		set_field_props(frm, VICTIM_FIELDS, 0, 0);
+		frm.set_df_property('role_type', 'reqd', 1);
+		
+		const role = frm.doc.role_type;
+		const configs = {
+			'Employee (FTE)': { show: ['victim_employee', 'victim_employee_department'], hide: ['victim', 'department'] },
+			'Casual': { show: ['victim', 'department'], hide: ['victim_employee', 'victim_employee_department'] },
+			_default: { show: ['victim'], hide: ['victim_employee', 'victim_employee_department', 'department'] }
+		};
+		const config = configs[role] || configs._default;
+		
+		config.show.forEach(f => {
+			frm.set_df_property(f, 'hidden', 0);
+			frm.set_df_property(f, 'reqd', 1);
+		});
+		config.hide.forEach(f => {
+			frm.set_df_property(f, 'hidden', 1);
+			frm.set_df_property(f, 'reqd', 0);
+		});
+		['victim', 'victim_employee', 'victim_employee_department', 'department'].forEach(f => frm.refresh_field(f));
+		
+		frm.set_df_property('victim_presence_status', 'reqd', 1);
+		frm.set_df_property('ppe_status', 'reqd', 1);
 	}
 }
 
 frappe.ui.form.on("Incident Report", {
+	fetch_geolocation: (frm) => {
+		hrms.fetch_geolocation(frm);
+	},
 	onload: function(frm) {
-		update_severity_options(frm);
-		// On load, if incident_type exists and severity is blank, set default
-		const mapping = {
-			"First Aid Case (FAC)": "Moderate",
-			"Lost Time Injury (LTI)": "High",
-			"Fatality": "Catastrophic",
-		};
-		const sev = mapping[frm.doc.incident_type];
+		toggle_victim_fields(frm);
+		const sev = SEVERITY_MAP[frm.doc.incident_type];
 		if (sev && !frm.doc.severity) {
 			frm.set_value('severity', sev);
 		}
 	},
 	incident_type: function(frm) {
-		update_severity_options(frm);
-		// Always set mapped severity on type change (overwrite existing)
-		const mapping = {
-			"First Aid Case (FAC)": "Moderate",
-			"Lost Time Injury (LTI)": "High",
-			"Fatality": "Catastrophic",
-		};
-		const sev = mapping[frm.doc.incident_type];
+		toggle_victim_fields(frm);
+        
+        if (frm.doc.incident_type === 'Others') {
+            frm.set_value('role_type', '');
+            VICTIM_FIELDS.forEach(f => frm.set_value(f, ''));
+        }
+
+		const sev = SEVERITY_MAP[frm.doc.incident_type];
 		if (sev) {
 			frm.set_value('severity', sev);
 		}
-	}
+	},
+	incident_type_subcategory: toggle_victim_fields,
+	role_type: toggle_victim_fields
 });
